6d97209cd58ff0f2920cafcf05a5f49988d60441
유인호 <yuiop1029@naver.com>
2024-01-11 09:55:13 +0900

    init

---
+++ SeSAC Week 4/Movie/ViewController/MovieViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MovieViewController.swift
+//  SeSAC Week 4
+//
+//  Created by A_Mcflurry on 1/11/24.
+//
+
+import UIKit
+
+class MovieViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		view.backgroundColor = .black
+	}
+}
---
+++ SeSAC Week 4/Second/SecondViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SecondViewController.swift
+//  SeSAC Week 4
+//
+//  Created by A_Mcflurry on 1/11/24.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
